c4d65faa3027faffca6188e50d2328b6ca95cbbe
Romina Dehesa <rominadehesa@gmail.com>
2025-10-14 18:05:22 -0300

    fix

--- src/components/general/Footer.svelte
+++ src/components/general/Footer.svelte
@@ -19,7 +19,7 @@
         <div class="social">
             <div>
                 <!-- svelte-ignore a11y_consider_explicit_label -->
-                <a href="">
+                <a href="https://api.whatsapp.com/send/?phone=5492983554828&text&type=phone_number&app_absent=0">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="none">
                         <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.3789 2.27907 14.6926 2.78382 15.8877C3.06278 16.5481 3.20226 16.8784 3.21953 17.128C3.2368 17.3776 3.16334 17.6521 3.01642 18.2012L2 22L5.79877 20.9836C6.34788 20.8367 6.62244 20.7632 6.87202 20.7805C7.12161 20.7977 7.45185 20.9372 8.11235 21.2162C9.30745 21.7209 10.6211 22 12 22Z" stroke="#F4F4F4" stroke-width="1.5" stroke-linejoin="round"></path>
                         <path d="M8.58815 12.3773L9.45909 11.2956C9.82616 10.8397 10.2799 10.4153 10.3155 9.80826C10.3244 9.65494 10.2166 8.96657 10.0008 7.58986C9.91601 7.04881 9.41086 7 8.97332 7C8.40314 7 8.11805 7 7.83495 7.12931C7.47714 7.29275 7.10979 7.75231 7.02917 8.13733C6.96539 8.44196 7.01279 8.65187 7.10759 9.07169C7.51023 10.8548 8.45481 12.6158 9.91948 14.0805C11.3842 15.5452 13.1452 16.4898 14.9283 16.8924C15.3481 16.9872 15.558 17.0346 15.8627 16.9708C16.2477 16.8902 16.7072 16.5229 16.8707 16.165C17 15.8819 17 15.5969 17 15.0267C17 14.5891 16.9512 14.084 16.4101 13.9992C15.0334 13.7834 14.3451 13.6756 14.1917 13.6845C13.5847 13.7201 13.1603 14.1738 12.7044 14.5409L11.6227 15.4118" stroke="#F4F4F4" stroke-width="1.5"></path>
@@ -52,14 +52,15 @@
         <ul>
             <li><a href="/">Inicio</a></li>
             <li><a href="/home#sobrenosotros">Nuestra historia</a></li>
-            <li><a href="#/servicios">Servicios</a></li>
-            <li><a href="#/contacto">Clientes</a></li>
+            <li><a href="/#/servicios">Servicios</a></li>
+            <li><a href="javascript:void(0)" on:click={goToForm}>Contacto</a></li>
         </ul>
 
         <ul>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Email</a></li>
-            <li><a href="">WhatsApp</a></li>
+            <li><a href="https://www.instagram.com/drf.montajes.industriales/">Instagram</a></li>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <li><a href="javascript:void(0)" on:click={goToForm}>Email</a></li>
+            <li><a href="https://api.whatsapp.com/send/?phone=5492983554828&text&type=phone_number&app_absent=0">WhatsApp</a></li>
         </ul>
     </div>
 </footer>
--- src/routes/Contacto.svelte
+++ src/routes/Contacto.svelte
@@ -22,15 +22,13 @@
         <h1>Trabajamos en  Tres Arroyos y en todo el país.</h1>
         <p>Consultanos hoy mismo y coordiná tu proyecto con nuestro equipo.</p>
         <div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" color="#fff" fill="none">
-                    <path d="M14 22V8C14 5.17157 14 3.75736 13.1213 2.87868C12.2426 2 10.8284 2 8 2C5.17157 2 3.75736 2 2.87868 2.87868C2 3.75736 2 5.17157 2 8V16C2 18.8284 2 20.2426 2.87868 21.1213C3.75736 22 5.17157 22 8 22H14Z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-                    <path d="M6.5 11H5.5M10.5 11H9.5M6.5 7H5.5M6.5 15H5.5M10.5 7H9.5M10.5 15H9.5" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-                    <path d="M18.5 15H17.5M18.5 11H17.5" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-                    <path d="M18 8H14V22H18C19.8856 22 20.8284 22 21.4142 21.4142C22 20.8284 22 19.8856 22 18V12C22 10.1144 22 9.17157 21.4142 8.58579C20.8284 8 19.8856 8 18 8Z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-                </svg>
-            <div >
-                <p>Avenida Belgrano 1040, Tres Arroyos, Buenos Aires, Argentina. <br> De Lunes a Viernes, de 8:00hs a 16:00 hs. Sábados, de 8:00hs a 12:00hs.</p>
-            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" color="#fff" fill="none">
+                <path d="M14 22V8C14 5.17157 14 3.75736 13.1213 2.87868C12.2426 2 10.8284 2 8 2C5.17157 2 3.75736 2 2.87868 2.87868C2 3.75736 2 5.17157 2 8V16C2 18.8284 2 20.2426 2.87868 21.1213C3.75736 22 5.17157 22 8 22H14Z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M6.5 11H5.5M10.5 11H9.5M6.5 7H5.5M6.5 15H5.5M10.5 7H9.5M10.5 15H9.5" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M18.5 15H17.5M18.5 11H17.5" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M18 8H14V22H18C19.8856 22 20.8284 22 21.4142 21.4142C22 20.8284 22 19.8856 22 18V12C22 10.1144 22 9.17157 21.4142 8.58579C20.8284 8 19.8856 8 18 8Z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+            <p>Avenida Belgrano 1040, Tres Arroyos, Buenos Aires, Argentina. <br> De Lunes a Viernes, de 8:00hs a 16:00 hs. Sábados, de 8:00hs a 12:00hs.</p>
         </div>
         <br>
         <div>
@@ -58,12 +56,12 @@
         align-items: center;
         gap: 150px;
     }
-    section div {
+    section > div {
         width: 45%;
     }
 
     .div-info > div {
-        width: 90%;
+        width: 100%;
         display: flex;
         justify-content: start;
         align-items: start;
